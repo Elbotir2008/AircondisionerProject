adac3a9afb509e879c8b84ff05c0b757299acc9a
Elbotir2008 <elbotir2008@gmail.com>
2024-09-18 22:56:03 +0500

    df

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,16 +12,34 @@ const Home = () => {
     <div>
       <section className="hero-section" id="hero">
         <div className="container">
-          {/* <h1>Охлаждаем Ваш дом с максимальным комфортом в СПБ</h1>
+          <h1>Охлаждаем Ваш дом с максимальным комфортом в СПБ</h1>
           <p>
-            широкий выбор кондиционеров от ведущих производителей нашего
+            Широкий выбор кондиционеров от ведущих производителей нашего
             магазина
           </p>
-          <button>Подробнее</button> */}
+          <button>Подробнее</button>
         </div>
       </section>
       <section className="condisioner-section">
-        <div className="container"></div>
+        <div className="container">
+          <div className="condisionerTitle flex-class">
+            <div className="condisionerTitleLeft">
+              <h1>Лучший выбор</h1>
+              <div className="lineh1"></div>
+            </div>
+            <div className="condisionerTitleRight">
+              <div className="lineh2"></div>
+              <h2>Кондиционеры</h2>
+            </div>
+          </div>
+          <div className="condisionerCards">
+            {dataApi.map((item) => (
+              <div className="condisionerCard" key={item.id}>
+                <h1>{item.name}</h1>
+              </div>
+            ))}
+          </div>
+        </div>
       </section>
       <section className="company-section" id="company">
         <div className="container"></div>
